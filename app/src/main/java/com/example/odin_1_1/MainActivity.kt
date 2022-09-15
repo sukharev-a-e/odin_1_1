27fff3f74e2192e62895567ec87c1b5ec6d665c3
sukharev-a-e <sukharev-a-e@rambler.ru>
2022-09-15 18:43:53 +0300

    Initial fixation

---
+++ app/src/main/java/com/example/odin_1_1/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.odin_1_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val x: Int = 3
+        val y: Int = 4
+        val z: Int = 5
+        val volume = x * y * z
+
+        //val FirstText = findViewById<TextView>(R.id.volume)
+        val FirstText: TextView
+        FirstText = findViewById(R.id.volume)
+
+        FirstText.text = "Объем параллелепипеда со сторонами $x, $y и $z равен $volume."
+    }
+
+
+
+}
